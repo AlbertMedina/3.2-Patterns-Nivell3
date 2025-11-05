40fd5eae5a013ecc8cf9e094ef1d3493f345a31c
Albert Medina <albertmecu@gmail.com>
2025-11-05 10:35:55 +0100

    feat: added stocks state + testing from main

--- src/main/java/level2/Main.java
+++ src/main/java/level2/Main.java
@@ -2,6 +2,11 @@ package level2;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Stockbroker stockbroker = new Stockbroker();
+
+        stockbroker.addBrokerage(new BrokerageA());
+        stockbroker.addBrokerage(new BrokerageB());
+        stockbroker.updateStocksState("Stocks up!");
     }
 }
--- src/main/java/level2/Stockbroker.java
+++ src/main/java/level2/Stockbroker.java
@@ -7,15 +7,26 @@ public class Stockbroker {
 
     private final List<Brokerage> brokerages = new ArrayList<>();
 
+    private String stocksState;
+
     public List<Brokerage> getBrokerages() {
         return List.copyOf(brokerages);
+    }
+
+    public String getStocksState() {
+        return stocksState;
     }
 
     public void addBrokerage(Brokerage brokerage) {
         brokerages.add(brokerage);
     }
 
-    public void notifyBrokerages(String message) {
+    public void updateStocksState(String state) {
+        stocksState = state;
+        notifyBrokerages(stocksState);
+    }
+
+    private void notifyBrokerages(String message) {
         brokerages.forEach(b -> b.update(message));
     }
 }
